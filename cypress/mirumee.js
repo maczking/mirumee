2c68cf083e8c5e6c68a6a582ac20004ba36ea004
Inga Mączka <inga.maczka@wfirma.pl>
2021-11-15 00:41:41 +0100

    usunięcie

--- cypress/mirumee.js
+++
@@ -1,8 +0,0 @@
-context('Add shoes to basket', () => {
-    beforeEach(() => {
-      cy.visit('https://demo.saleor.io/')
-    })
-        it('going to shoes page', () => {
-        cy.trigger('.main-menu__left')
-        .should('have.value', 'GROCERIES')
-      })
